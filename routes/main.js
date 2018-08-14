40d2660c7ea09584cb3e8a7224afd47c67c7e223
Hai Truong <ximbong91023@yahoo.com>
2018-08-14 12:53:06 +0300

    Modify main route

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,23 @@
 const express = require("express"),
   router = express.Router(),
-  Post = require("../models/post");
+  Post = require("../models/post"),
+  Featured = require("../models/featured");
 
 router.get("/", function(req, res) {
-  Post.find({}, function(err, posts) {
-    err ? console.log(err) : res.json(posts);
-  });
+  const response = {};
+  Post.find({})
+    .sort({ _id: -1 })
+    .limit(20)
+    .exec(function(err, posts) {
+      response.main = posts;
+      Featured.find({})
+        .limit(7)
+        .exec(function(err, featured) {
+          console.log(featured);
+          response.featured = featured;
+          res.json(response);
+        });
+    });
 });
 
 module.exports = router;
