48685ba4ce96993f629259738bbc4de002125e9a
ximbong91023 <ximbong91023@yahoo.com>
2018-08-16 00:41:29 +0300

    Fix featured post model

--- models/featured.js
+++ models/featured.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const featuredSchema = new Schema({
-  posts: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+  data: { type: Schema.Types.ObjectId, ref: "Post" }
 });
 
 module.exports = mongoose.model("Featured", featuredSchema);
